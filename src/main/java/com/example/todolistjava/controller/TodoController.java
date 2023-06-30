8ff4a74ae3ae8ec65a585f654fd59ac21c083f5f
SoohoPark <clapp301@gmail.com>
2023-06-30 14:56:47 +0900

    프론트엔드 백엔드 연결 - 초기값 불러오기

---
+++ src/main/java/com/example/todolistjava/controller/TodoController.java
@@ -0,0 +1,29 @@
+package com.example.todolistjava.controller;
+
+import com.example.todolistjava.domain.Todo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+public class TodoController {
+    @GetMapping("/api")
+    @ResponseBody
+    public List<Todo> getTodo() {
+        List<Todo> list = new ArrayList<>();
+        Todo todo1 = new Todo("1", "a", false);
+        Todo todo2 = new Todo("2", "b", false);
+        Todo todo3 = new Todo("3", "c", false);
+
+        list.add(todo1);
+        list.add(todo2);
+        list.add(todo3);
+
+        return list;
+    }
+
+}
