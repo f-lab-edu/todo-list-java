7ad0b7679f436ffcba22de142a243597f5e8bf7a
SoohoPark <clapp301@gmail.com>
2023-08-17 14:29:21 +0900

    @RequestParam을 @RequestBody로 변경

--- src/main/java/com/example/todolistjava/controller/TodoController.java
+++ src/main/java/com/example/todolistjava/controller/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
@@ -27,21 +27,13 @@ public class TodoController {
     }
 
     @PostMapping("/todos")
-    public Todo createOne(
-            @RequestParam("id") String id,
-            @RequestParam("text") String text,
-            @RequestParam("isCompleted") boolean isCompleted,
-            @RequestParam("isEdit") boolean isEdit) throws SQLException {
-
-        Todo todo = new Todo(id, text, isCompleted, isEdit);
+    public Todo createOne(@RequestBody Todo todo) throws SQLException {
         return todoService.addTodo(todo);
     }
 
     @PatchMapping("/todos")
-    public void updateOne(
-            @RequestParam String id,
-            @RequestParam String text) throws SQLException {
-        todoService.editTodo(id, text);
+    public void updateOne(@RequestBody Todo todo) throws SQLException {
+        todoService.editTodo(todo.getId(), todo.getText());
     }
 
     @DeleteMapping("/todos/{id}")
